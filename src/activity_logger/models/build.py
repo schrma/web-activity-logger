6cc6156edd1add913d3c1fd98735aaee746e02df
ms <mm4go.marcschr@dfgh.net>
2023-12-04 19:35:35 +0100

    Stylecheck

--- src/activity_logger/models/build.py
+++ src/activity_logger/models/build.py
@@ -1,6 +1,7 @@
 from activity_logger.models.db_activites import init_activities
 from activity_logger.models.org import init_user
 
+
 def build_default_database():
     init_user()
     init_activities()
